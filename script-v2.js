06e8590b98c4657529a1a174fbeb9cba668ec37d
Catita25 <p4m7h8ydfk@privaterelay.appleid.com>
2025-12-18 01:04:33 -0300

    Update script-v2.js

--- script-v2.js
+++ script-v2.js
@@ -19,7 +19,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
   ];
 
-
   function puedeDesbloquear(ramo) {
     return ramo.prerequisitos.every(id =>
       ramos.find(r => r.id === id && r.aprobado)
